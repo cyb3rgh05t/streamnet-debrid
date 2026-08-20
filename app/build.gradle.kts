e357fb6e2ead09a5310f0a92ab29e39dd4ba1790
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2026-08-20 08:47:34 +0200

    chore(android): bump version to 2.0.028

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         // Fire TV devices can be as low as Android 7.1 (API 25) or lower depending on model/OS.
         minSdk = 23
         targetSdk = 36
-        versionCode = 330
-        versionName = "2.0.027"
+        versionCode = 331
+        versionName = "2.0.028"
         buildConfigField("String", "GITHUB_OWNER", "\"cyb3rgh05t\"")
         buildConfigField("String", "GITHUB_REPO", "\"streamnet-debrid\"")
         buildConfigField("Boolean", "FEATURE_PLUGINS_ENABLED", "false")
