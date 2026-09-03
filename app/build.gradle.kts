a85aa45e34d4c0d066003f2909925c4aa85be9b4
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2026-09-03 17:04:48 +0200

    v2.2.006

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         // Fire TV devices can be as low as Android 7.1 (API 25) or lower depending on model/OS.
         minSdk = 23
         targetSdk = 36
-        versionCode = 380
-        versionName = "2.2.005"
+        versionCode = 381
+        versionName = "2.2.006"
         buildConfigField("String", "GITHUB_OWNER", "\"cyb3rgh05t\"")
         buildConfigField("String", "GITHUB_REPO", "\"streamnet-debrid\"")
         buildConfigField("Boolean", "FEATURE_PLUGINS_ENABLED", "false")
